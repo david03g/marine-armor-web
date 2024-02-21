8ad7af76edb2de768064104eeb9e673ba48bc12b
david03g <dgoldstein.us@gmail.com>
2024-02-21 00:43:53 -0800

    playsinline

--- src/components/VideoHero.js
+++ src/components/VideoHero.js
@@ -3,7 +3,7 @@ function VideoHero({src, overlaycontent}) {
   return (
 
     <div class="video-background" >
-        <video autoPlay loop muted>
+        <video autoPlay loop muted playsinline>
             <source src={src} type="video/mp4"/> 
             Your browser does not support the video tag.
         </video>
