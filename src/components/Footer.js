42f5d2f18c12166cc1cb1c2fa3332980c87d3f4c
david03g <dgoldstein.us@gmail.com>
2024-02-26 13:13:47 -0800

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,18 @@
-
 function Footer({}) {
-    return (
-  
-        <div style={{paddingBottom:0}}>
-        <div>
-            <div style={{padding:20,backgroundColor:'#f4f4fc',  }}>
-            
-           
-            <p style={{fontSize:30, textAlign:'left', color:'#717274', fontWeight:'bold'}}></p>
-          
-                    <p style={{width:'80%', fontWeight:'bold', color:"#717274", textAlign:'left'}}>
-        
-                     {/* Marine Armor is a new technology to protect endangered and threatened marine mammals from vessel strikes. */}
-                    </p>
-                     <div style={{display:'flex', alignItems:'flex-end', flexDirection:'row', justifyContent:'space-between', justifyContent:'right'}}>
-                      {/* <Button variant='contained' style={{height:40}}>Learn More</Button> */}
-                    {/* <img className='App-logo' height={80} width={80} src={wheel}/> */}
-                    </div>
-        
-          </div>
-        
+  return (
+      <div style={{ paddingBottom: 0 }}>
+          <div>
+              <div style={{ padding: 20, backgroundColor: '#f4f4fc' }}>
+                  {/* You can include any other elements or text above the copyright notice as needed */}
+                  
+                  {/* Copyright notice */}
+                  <p style={{ width: '100%', fontWeight: 'bold', color: "#717274", textAlign: 'center', marginTop: '16px' }}>
+                      © {new Date().getFullYear()} Copyright Resonance Labs
+                  </p>
               </div>
-        
-            </div>
-        
-  
-  
-    );
-  }
-  
-  
-  
-  export default Footer;
-  
+          </div>
+      </div>
+  );
+}
 
+export default Footer;
